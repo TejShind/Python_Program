da6c11f8445083c58d9fda56f132b804843e561c
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-28 16:43:02 +0530

    [Tejaswini]Add: Refactor the code add doc string structure

--- Power_Of_Two.py
+++ Power_Of_Two.py
@@ -3,9 +3,10 @@
 @Date: 2022-04-27 22.49
 @Last Modified by: Tejaswini Shinde
 @Last Modified time: None
-@Title : Leap Year
+@Title : Power Of two
 """
-"""
+def power_of_two():
+    """
     Description: 
        It gives the power of two of the numbers till given input number
     Parameter:
@@ -13,15 +14,10 @@
     Return:
         None
 """
-
-  
-
-def power_of_two():
      
     number = int(input("Enter the number between 0 and less than 31: "))
 
     for i in range(number+1):
         print(2 ** i)
-
 
 power_of_two()
