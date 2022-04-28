cbd7aa0ed655ffa06d74884b4c5d19995a961ae2
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-28 13:29:35 +0530

    [Tejaswini]Add: Refactor the code with while loop

--- User_Name.py
+++ User_Name.py
@@ -6,22 +6,24 @@
 @Title :Printing the String with User Name
 """
 
-"""
-  Description:
-            Below function asks for the username and greet with the username
+
+def user_name():
+    """
+        Description:
+           Below function asks for the username and greet with the username        
         Parameter:
             None
         Return:
             None
+    """
 
+    print("Hello,How Are You! ")
 
-"""
-
-
-def user_name():
-    name = input("Enter the name :")
-    if len(name) >= 3:
-        print(f"Hello {name}, how are you ?")
-
+    name = input("Enter the user name :\n")
+    while True:
+      if len(name) < 3:
+         name=input("Entert valide user name with minimum three character : \n")
+      else:
+          break
 
 user_name()
