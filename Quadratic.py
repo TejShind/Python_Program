344450c464b29fc427f56e3bd5e2fd6fdbe17f9a
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-29 22:28:20 +0530

    [Tejaswini]Add : Calculate roots by using quadratic equation

---
+++ Quadratic.py
@@ -0,0 +1,36 @@
+"""
+@Author: Tejaswini Shinde
+@Date: 2022-04-29 19:00 
+@Last Modified by: Tejaswini Shinde
+@Last Modified time: None
+@Title : Calculate distance between of x and y co-ordinator from origin
+
+"""
+import math
+
+if __name__ == '__main__':
+
+    a=int(input("Enter a Value of a"))
+    b=int(input("Enter a Value of b"))
+    c=int(input("Enter a Value of c"))
+
+    delta = ((b*b)-4*a*c)
+    print("delta",delta)
+
+    if (delta < 0):
+        print("Roots are Imaginary")
+    elif (delta >0 ):
+        root1 = (-b + math.sqrt(delta / (2 * a)))
+        root2 = (-b - math.sqrt(delta / (2 * a)))
+        print("Root 01 Is: ", root1)
+        print("Root 02 Is: ", root2)
+
+    elif (delta == 0):
+        root1 = (-b / 2 * a)
+        root2 = (-b / 2 * a)
+        print("Root 01 Is: ", root1)
+        print("Root 02 Is: ", root2)    
+    
+
+
+        
