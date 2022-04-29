85e031d348e6d2d8dc630d7adde07984f5626619
unknown <Komal@DESKTOP-2EFH2MU.(none)>
2022-04-29 14:55:18 +0530

    [Tejaswini]Add : Refactor code take input from user

--- Power_Of_Two.py
+++ Power_Of_Two.py
@@ -2,22 +2,25 @@
 @Author: Tejaswini Shinde
 @Date: 2022-04-27 22.49
 @Last Modified by: Tejaswini Shinde
-@Last Modified time: None
+@Last Modified time: 2022-04-29
 @Title : Power Of two
 """
-def power_of_two():
+def power_of_two(n):
     """
     Description: 
        It gives the power of two of the numbers till given input number
     Parameter:
-        None
+        Passing power value of n
     Return:
-        None
-"""
-     
-    number = int(input("Enter the number between 0 and less than 31: "))
-
+        Returning power of 2 until n value
+    """
     for i in range(number+1):
-        print(2 ** i)
+        print(2 ** i) 
 
-power_of_two()
+
+number = int(input("Enter the number of time you want to calculate power: "))
+if (number <= 0) or (number >31):
+    print("Enter a valid Range")
+
+else:
+       power_of_two(number)
